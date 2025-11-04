887569333a8d077423c4a1bcbc041d11fa9ce3b3
Leon-Ogunfeyimi <20108255@mail.wit.ie>
2025-11-04 01:42:01 +0000

    Exercise: Movie Details Page- included production countries.

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -60,6 +60,20 @@ const [drawerOpen, setDrawerOpen] = useState(false);
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+            <Paper component="ul" sx={{ ...root }}>
+        <li>
+          <Chip
+            label="Production Countries"
+            sx={{ ...chip }}
+            color="primary"
+          />
+        </li>
+        {movie.production_countries.map((c) => (
+          <li key={c.name}>
+            <Chip label={c.name} sx={{ ...chip }} />
+          </li>
+        ))}
+      </Paper>
       <Fab
         color="secondary"
         variant="extended"
